32e088e196b6bb2f97fb7e8b2b86e9bac3dbd332
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-10-31 09:37:08 -0700

    print

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,21 +8,21 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new-node;
+	binary_tree_t *new_node;
 
 	if (parent == NULL)
 		return (NULL);
 
-	new-node = malloc(sizeof(binary_tree_t));
-	if (!new-node)
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
 		return (NULL);
 
-	new-node->n = value;
-	new-node->parent = parent;
-	new-node->right = NULL;
-	new-node->left = parent->left;
-	parent->left = new-node;
-	if (new-node->left)
-		new-node->left->parent = new-node;
-	return (new-node);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->right = NULL;
+	new_node->left = parent->left;
+	parent->left = new_node;
+	if (new_node->left)
+		new_node->left->parent = new_node;
+	return (new_node);
 }
