54f83d183d1e1347c3bc816d8b5126259df5cbc4
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-10-31 17:02:18 -0700

    End

--- binary_trees.h
+++ binary_trees.h
@@ -58,4 +58,5 @@ size_t binary_tree_height_balance(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 int _pow_recursion(int x, int y);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 #endif
